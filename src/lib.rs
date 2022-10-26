e21e2a1260357b4a28dc248dd9fc83f0fb0b65ee
AmauMaill <amaury.maill@gmail.com>
2022-10-27 00:58:47 +0200

    Move code to lib, separation of concerns

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::{ fs };
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(
+        args: &[String]
+    ) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("missing arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(
+    config: Config
+) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(
+        config.file_path
+    )?;
+
+    println!("With test:\n{}", contents);
+
+    Ok(())
+}
